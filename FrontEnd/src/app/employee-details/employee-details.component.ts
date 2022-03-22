ab868446b6a432fe4e980d93f70401bab6fd3877
Mohamed Osama <mohamed.osama@arpuplus.com>
2022-03-23 00:06:46 +0200

    simple Front

--- FrontEnd/src/app/employee-details/employee-details.component.ts
+++ FrontEnd/src/app/employee-details/employee-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Employee} from "../model/employee";
+import {ActivatedRoute} from "@angular/router";
+import {EmployeeService} from "../servic/emplyee.service";
 
 @Component({
   selector: 'app-employee-details',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number
+  employee: Employee
+  constructor(private route: ActivatedRoute, private employeService: EmployeeService) { }
 
   ngOnInit(): void {
-  }
+    this.id = this.route.snapshot.params['id'];
 
+    this.employee = new Employee();
+    this.employeService.getEmployeeById(this.id).subscribe( data => {
+      this.employee = data;
+    });
+  }
 }
